024767641812b44845edb01ee25f8efdbfa3fa44
Tapaswini Nayak <92291172+TapaswiniNayak@users.noreply.github.com>
2023-05-08 22:04:06 +0530

    Update neotrek.py

--- neotrek.py
+++ neotrek.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr # recognise speech
-import playsound # to play an audio file
+import playsound # to play an audio file 
 from gtts import gTTS # google text to speech
 import random
 from time import ctime # get time details
